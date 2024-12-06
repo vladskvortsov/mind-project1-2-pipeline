8a15206c6927f197686002f36a4fcaf167034b70
vladskvortsov <vladskvortsov96@gmail.com>
2024-12-06 13:11:15 +0200

    deploy-project action added

---
+++ terraform-frontend/main.tf
@@ -0,0 +1,18 @@
+module "deploy-cloudfront" {
+    source = "../terraform/cloudfront.tf"
+
+
+}
+
+module "deploy-s3" {
+    source = "../terraform/s3.tf"
+
+
+}
+
+
+module "deploy-s3" {
+    source = "../terraform/s3.tf"
+
+
+}
