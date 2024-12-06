c601a18698143ae8ea1d1d25c208eef4684bb0d5
vladskvortsov <vladskvortsov96@gmail.com>
2024-12-06 14:48:39 +0200

    destroy-resources action added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -25,7 +25,3 @@ output "cloudfront_distribution_domain_name" {
 output "ec2_instance_ip" {
   value = module.ec2_instance[0].public_ip
 }
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
