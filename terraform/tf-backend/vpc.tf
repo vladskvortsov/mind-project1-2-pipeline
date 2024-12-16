24321215046a3095d5c4f1a4485c6ce50c435620
vladskvortsov <vladskvortsov96@gmail.com>
2024-12-16 15:06:41 +0200

    ssh fix

--- terraform/tf-backend/vpc.tf
+++ terraform/tf-backend/vpc.tf
@@ -23,10 +23,10 @@ module "ec2_sg" {
   vpc_id = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+  ingress_rules       = ["http-80-tcp"]
 
   ingress_with_cidr_blocks = [
-    
+
     {
       from_port   = 22
       to_port     = 22
