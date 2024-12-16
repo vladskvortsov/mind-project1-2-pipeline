0e646ef8ea883ccfee9eea691fcfe7271a555013
vladskvortsov <vladskvortsov96@gmail.com>
2024-12-16 21:08:20 +0200

    ssm connect added

--- terraform/tf-backend/ec2.tf
+++ terraform/tf-backend/ec2.tf
@@ -59,7 +59,7 @@ data "aws_iam_policy_document" "ec2-connect" {
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::194722414339:user/user","arn:aws:sts::194722414339:assumed-role/ec2-connect/GitHubActions"]
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/user","arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/ec2-connect/ec2-connect"]
     }
 
     actions = ["sts:AssumeRole", "sts:TagSession"]
